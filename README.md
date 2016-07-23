7ba7874f94935dbb99718268f051a3b11a59b769
Austin Brown <Austin Brown>
2016-07-23 15:12:34 -0400

    added alexa schema and got code working

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# BikeshareAlexa
+# Bikeshare for Alexa
 Works with Amazon Echo to tell me how many Capital Bikeshare bikes are nearby
+
+By default, this uses a dictionary of my closest stops
+
+## Reuse
+You are welcome to reuse this code.
+
+### Resources
+You will need to get a (free) developer setup with Amazon:  
+https://developer.amazon.com/home.html
+and an AWS account (also free for I think a million calls a month)  
+https://console.aws.amazon.com/console/home?region=us-east-1  
+note that you must select the us-east-1 region for Alexa skills  
+
+### Then to use the code:  
+**Code:**  
+* Change the station list in the getspeech() function to the list of stations and you want to query with your preferred name for each
+
+**Lambda:**  
+* Go to the AWS console and select "lambda"  
+* Click "Create a Lambda function"  
+* Skip selecting a blueprint by clicking next
+* in "Configure Triggers", click in the box and select Alexa Skills Kit
+* Name the function BikeshareAlexa, and add a description if you like
+* Select "python" as runtime  
+* Paste in the whole .py file  
+* Under "Role", select lambda_basic_execution  
+* you may want to turn up the timeout since we're hitting an API (I use 6 seconds)  
+* Click next  
+
+*Keep this open since you'll need the ARN from the top right shortly*
+
+**Alexa Skill:**
+* Go to developer.amazon.com and log in
+* Click "alexa" on the top bar
+* Click "add a new skill"
+* For name, use "WhenIsMyBus"
+* For invocation name use "when is my bus"
+* Click next
+* Paste the intent schema into the top box
+* Paste the sample utterances into the bottom box
+* Click save, it should build the model
+* Click next.
+
+*If you want to customize:*  
+Change the list of stop IDs and route names to the ones that interest you.
+
+That's it! Your echo should have the skill immediately. Just say:  
+*Alexa, ask when is my bus*
--- bikeshare_alexa.py
+++ bikeshare_alexa.py
@@ -8,10 +8,10 @@ An alexa app to check on the status of nearby capital bikeshare stations
 """
 
 import xml.etree.ElementTree as ET
-import httplib, urllib, urllib2, base64
+import httplib
 
 
-def Get_station_data():
+def get_station_data():
     # get the xml as a string
     try:
         conn = httplib.HTTPSConnection('www.capitalbikeshare.com')
@@ -23,15 +23,165 @@ def Get_station_data():
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
         
     # put it into an ET object
-    root = ET.fromstring(data)
+    root = ET.fromstring(data)    
     return root
+    
+def get_bikes_dict():
+    # get the xml
+    root = get_station_data()
+    
+    #iterate through the stations and build a dict of just the terminalName
+    bikes_dict = {}
+    for station in root:
+        bikes_dict[int(station.find('terminalName').text)] = int(station.find('nbBikes').text)
+    
+    return bikes_dict
+
+#custom get speech function to return the speech for bikeshare
+def get_speech():
+
+    # define a list of stops and plain language names in the order
+    # that you want Alexa to read them.
+    station_list = [[31115, "Columbia"],[31102,"Kenyon"],[31105,"Harvard"],[31207,"Fairmont"]]
+
+    # get the bikes counts
+    bikes_dict = get_bikes_dict()
+
+    # for each station we're looking for, look up the number of bikes in the bikes_dict
+    results_list = []
+    for station, name in station_list:
+        results_list.append([name,bikes_dict[station]])
+    
+    speech_output = "Nearby bikes: "
+    for name, bikes in results_list:
+        speech_output += str(bikes) + " bike"
+        if bikes != 1:
+            speech_output += "s"
+        speech_output += " at " + str(name) + ", "
+    
+    
+    return speech_output
+    
+    
+# ---------------------- ALEXA FUNCTIONS ------------------#
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']}, event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
 
 
-# some user parameters
-Read_empty_stations = True
-Read_number_of_docks = False
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
 
-# define a dictionary of stops and plain language names in the order
-# that you want Alexa to read them.
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
 
-station_dict = {31115: "Columbia", 31102: "Kenyon", 31105: "Harvard", 31207: "Fairmont"}
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill 
+    For now, we aren't using any intents    
+    """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "BikeshareAlexa":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    #elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+    #    return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+    
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = get_speech()
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "I don't think this should be read..."
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
